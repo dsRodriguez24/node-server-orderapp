b206b50d7f402321c72224d1a5dd10ef1adb2675
Santiago <santiago.salasdezoom@gmail.com>
2025-01-31 10:28:37 -0500

    Agregar las variables de entorno a configuracion de typeorm

--- db/db.connect.ts
+++ db/db.connect.ts
@@ -3,11 +3,11 @@ import { User, Rol, Product, Order } from "../entities";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "1234",
-    database: "db_pruebatecnica",
+    host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME ||  "postgres",
+    password: process.env.DB_PASSWORD ||  "1234",
+    database: process.env.DB_NAME ||  "db_pruebatecnica",
     synchronize: true,
     logging: true,
     subscribers: [],
